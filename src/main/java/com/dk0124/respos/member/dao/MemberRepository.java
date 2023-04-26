ba7b61bc84ccadf3258e393944a0b1c82fdccd0a
dkGithup2022 <104286091+dkGithup2022@users.noreply.github.com>
2023-04-26 20:14:23 +0900

    MemberRepository 오타 수정  : Long -> UUID

--- src/main/java/com/dk0124/respos/member/dao/MemberRepository.java
+++ src/main/java/com/dk0124/respos/member/dao/MemberRepository.java
@@ -5,11 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, UUID> {
 
     Optional<Member> findMemberByEmail(String email);
 
     Optional<Member> findMemberByNickName(String nickName);
+
 }
